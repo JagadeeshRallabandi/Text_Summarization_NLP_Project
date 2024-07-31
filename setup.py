c39341019122bd08d87c010ea2eb09f776d888ee
JagadeeshRallabandi <rjagadeeshnit@gmail.com>
2024-07-31 19:41:18 +0530

    Data_Ingestion

--- setup.py
+++ setup.py
@@ -1,30 +1,26 @@
-import setuptools
+from setuptools import find_packages,setup
+from typing import List
+HYPHEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    """
+    This function returns the list of requirements
 
-with open("README.md", "r", encoding="utf-8") as f:
-    long_description = f.read()
-
-
-__version__ = "0.0.0"
-
-REPO_NAME = "Text-Summarizer-Project"
-AUTHOR_USER_NAME = "entbappy"
-SRC_REPO = "textSummarizer"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
-
-
-
-setuptools.setup(
-    name=SRC_REPO,
-    version=__version__,
-    author=AUTHOR_USER_NAME,
-    author_email=AUTHOR_EMAIL,
-    description="A small python package for NLP app",
-    long_description=long_description,
-    long_description_content="text/markdown",
-    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    project_urls={
-        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
-    },
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src")
+    """
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+        
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+            
+    return requirements
+setup(
+    name="DataScienceProjects",
+    version="1.0",
+    author="Jagadeesh",
+    author_email="rjagadeeshnit@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+    
 )
